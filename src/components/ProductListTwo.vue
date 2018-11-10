72c00e0b70b5d8666f56dbdc5b58ad9041d85299
dkgian <duykhanhgian0510@gmail.com>
2018-11-10 19:59:48 +0100

    update product lists with props, simple style

--- src/components/ProductListTwo.vue
+++ src/components/ProductListTwo.vue
@@ -2,12 +2,19 @@
   <div id="product-list-two">
     <h2>Product list 2</h2>
     <ul>
+      <li v-for="product in products" v-bind:key="product.name">
+        <span class="name">{{ product.name }}</span>
+        <span class="price">{{ product.price }}</span>
+     </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
+  props: [
+    'products',
+  ],
   data () {
     return {
 
@@ -16,6 +23,26 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.price {
+  font-weight: bold;
+  color: brown;
+  display: block;
+}
+#product-list-two {
+  background: lightblue;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+  margin-bottom: 30px;
+  padding: 10px 20px;
+}
+#product-list-two ul {
+  padding: 0;
+  list-style-type: none;
+}
+#product-list-two li {
+  margin-right: 10px;
+  margin-top: 10px;
+  padding: 20px;
+  background: yellowgreen;
+}
 </style>
